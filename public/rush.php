82eadf17a76db2220252c27c8ea1ff2caecd0172
Kevin Jih <kjih@MacBook-Pro-3.local>
2016-09-06 20:28:16 -0700

    second event and change paphi houes address

--- public/rush.php
+++ public/rush.php
@@ -65,7 +65,7 @@
 
                 <div class="container-xs-left">
                     <p class="bodyText-left">Wednesday 9/21 - <strong>Don't Do Drugs Just Take Some &Pi;&Alpha;&Phi;</strong><br />Get to know PAPhi (Pronounced "P-A-Phi") and what we're all about.<br />Location: Pi Alpha Phi House<br />Time: 8PM</p>
-                    <p class="bodyText-left">Thursday 4/2 - <strong>&Pi;&Alpha;&Phi; Lil' Sis BBQ</strong><br />Come enjoy the famous PAPhi Chicken with our lovely Lil' Sis's! Arrive EARLY if you want to eat.<br />Time: 8PM</p>
+                    <p class="bodyText-left">Thursday 9/22 - <strong>T.G.I.F. BBQ (feat &Chi;&Delta;&Theta;)</strong><br />(Thank God It's Free) The best things in life are free.. Come out to our cook out (free of charge), &amp; get to know some of our best friends; the lovely ladies of Chi Delta Theta.<br />Location: 6542 Del Playa<br/>Time: 8PM</p>
                     <p class="bodyText-left">Saturday 4/4 - <strong>Wet N' Wild</strong><br />Join the brothers of Pi Alpha Phi at this Deltopia day rager full of music, dancing, and fun. Beach attire recommended. Warning: You may get wet.<br />Time: 1PM</p>
                     <p class="bodyText-left">Sunday 4/5 - <strong>Adopt-A-Block Philanthropy</strong><br />Work hard, play hard. Our fraternity is founded on the concept of helping others. Help us keep the streets of IV clean after Deltopia.<br />Time: 10AM</p>
                 </div>
@@ -74,7 +74,7 @@
                     <p class="bodyText-left">Thursday 4/9 - <strong>Info Night</strong><br />Come find out what we stand for, Greek life and how to join. Free pizza and drinks will be provided.<br />Time: 8PM</p>
                     <p class="bodyText-left">Wednesday 4/10 - <strong>Hypebeasts &amp; Hipster Geeks w/ &Chi;&Delta;&Theta;</strong><br />The name says it all. Dress to impress at this themed social with the radiant women of Chi Delta Theta.<br />Time: 9PM</p>
                     <p class="bodyText-left">Thursday 4/11 - <strong>Interviews &amp; Bid Night</strong><br />Come dressed in semi-formal clothes. Interview room TBA. Make sure you reserve a time with a brother during the week. If you forgot, message us before it's too late. *EXCLUSIVE* event for those extended bids.</p>
-                    <p class="bodyText-left">PAPhi House: 6632 Sabado Tarde Rd.<br /><br />For more information, questions, or concerns contact:<br /><br />Angel Esquivel: (951) 241-4683<br />Phu Nguyen: (916) 897-6903</p>
+                    <p class="bodyText-left">Pi Alpha Phi House: 6729 Sabado Tarde Rd.<br /><br />For more information, questions, or concerns contact:<br /><br />Angel Esquivel: (951) 241-4683<br />Phu Nguyen: (916) 897-6903</p>
 
                 </div>
             </div>
